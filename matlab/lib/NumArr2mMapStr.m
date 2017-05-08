98a087ae9ca1a46e71d7f3ddd27dd3fd3cf7e71d
Peter Mao <peterm@caltech.edu>
2017-05-08 16:23:17 -0700

    added documentation
    row or column vector ok as input (formerly, required column vector)

--- matlab/lib/NumArr2mMapStr.m
+++ matlab/lib/NumArr2mMapStr.m
@@ -1,17 +1,17 @@
 function output = NumArr2mMapStr(NumArr,mapSize)
-
+% converts numerical array into a string for input into xml
+% database file
+%
+% usage: output = NumArr2mMapStr(vector, mapSize=100)
+    
 if ~exist('mapSize','var'),mapSize=100;,end;
 
-NumArr = [NumArr; zeros(mapSize+2-length(NumArr),1)];
+NumArr = [NumArr(:); zeros(mapSize+2-length(NumArr),1)];
 
 mMapStr = '';
 
 for ii=1:length(NumArr)
-%     if ii==length(NumArr)
-%         mMapStr = [mMapStr num2str(NumArr(ii))];
-%     else
-        mMapStr = [mMapStr num2str(NumArr(ii)) ','];
-%     end
+    mMapStr = [mMapStr num2str(NumArr(ii)) ','];
 end
 
 
