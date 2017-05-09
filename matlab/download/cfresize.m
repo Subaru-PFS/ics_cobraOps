35d06299dd0fac8f83efa6d152d9a818f9aa0784
Peter Mao <peterm@caltech.edu>
2017-05-09 10:36:52 -0700

    Made a big paring down of the files in the matlab directory.  Confirmed that simFun still runs.

--- matlab/download/cfresize.m
+++
@@ -1,8 +0,0 @@
-function [im scale] = cfresize(im, longDimSize)
-%CFRESIZE resize the image if its long dimension is bigger then longDimSize 
-%   [im scale] = cfresize(im,longDimSize); scales the image so its long 
-%   dimension is logDimSize long. (only if its bigger)   
-
-s = size(im);
-scale = min(longDimSize/max(s),1);
-im = imresize(im, scale);
