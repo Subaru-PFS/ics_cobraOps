8bba6b290870c09e2d8802f4b0a24ea8a67f1184
Peter Mao <peterm@caltech.edu>
2017-05-24 16:29:17 -0700

    initial commit
    matlab script to load ETS input target list.  generates a summary figure.

---
+++ matlab/Target_Lists/pfs_target_test_data_set/data/load_dat.m
@@ -0,0 +1,32 @@
+function output=load_dat(filename)
+% loads a prelim data set
+
+% $$$   data format is:
+% $$$   1  ID
+% $$$   2  R.A.          [deg.]
+% $$$   3  Dec.          [deg.]
+% $$$   4  Exposure Time [sec.]
+% $$$   5  Priority      [1(highest)-15(lowest)]
+% $$$   6  Magnitude     [AB mag] (currently not available)
+% $$$   7  Redshift
+% $$$   8  Object Type
+
+   data_format = '%s %f %f %f %d %f %f %s';
+
+   
+   fid = fopen(filename);
+   contents = textscan(fid, data_format, 'TreatAsEmpty','N/A','CommentStyle','#');
+
+   fclose(fid);
+   
+   
+   output.ra  = contents{2};
+   output.dec = contents{3};
+
+   %% just for checking...
+   q = output;
+   plot((q.ra-mean(q.ra)).*cos(q.dec*pi/180)+mean(q.ra),q.dec, '.');
+   xlabel('RA "x cos(Dec)" [deg]');
+   ylabel('Dec [deg]');
+   title(filename,'Interpreter','none');
+   axis equal;
