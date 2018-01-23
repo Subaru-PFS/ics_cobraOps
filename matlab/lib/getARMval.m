f0d03fe638c9d72c2a8747fe123ba5b00e333b4c
Peter Mao <peterm@caltech.edu>
2018-01-23 10:48:34 -0800

    getARMval.m
    allow vector of pID's to be requested instead of a single pID.
    
    only a single mID is allowed.

--- matlab/lib/getARMval.m
+++ matlab/lib/getARMval.m
@@ -1,5 +1,17 @@
 function output=getARMval(CobraConfig, pID, mID, parameter)
-% get a numeric arm kinematics parameter
+% get a numeric arm kinematics parameter(s)
+%
+% Usage: output=getARMval(CobraConfig, pID, mID, parameter)
+% 
+% CobraConfig: data structure ready in by loadCfgXml.m
+% pID: positioner ID - may be an array
+% mID: module ID.  Always 1 for now, but in the future may be
+% something else
+% parameter: char array (old string)  argument name of parameter
+%
+% missing parameters lead to a listing of acceptable values and
+% null return value.
+
 output = [];
 ncobras = length(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER);
 %% generate arrays to help find container index for pid/mid
@@ -18,14 +30,19 @@ if ~exist('mID','var')
     disp(unique(mid));
     return
 end
-%% get the container index for the pID,mID requested
-kk = find(pID == pid & mID == mid,1);
-if isempty(kk)
+%% get the container indices for the [pID],mID requested
+mlocations = find(mID == mid);
+%kk = find(pID == pid & mID == mid,1);
+[junk_logical plocations] = ismember(pID,pid(mlocations)); % plocations are only the first location
+                                                           % where it matches, so we have to
+                                                           % filter on mids that match the request.
+containers = mlocations(plocations); % yes this is convoluted, but it allows for multiple values of mid.
+if isempty(containers)
    return 
 end
 if ~exist('parameter','var')
     fprintf(1,'Valid field names for pid %d mid %d:\n',pID,mID);
-    disp(fieldnames(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}.KINEMATICS))
+    disp(fieldnames(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{containers(1)}.KINEMATICS))
     return
 end
 
@@ -34,36 +51,21 @@ if(ischar(pID))
     pID = str2num(pID);
 end 
 
-%keyboard;
 if(isfield(CobraConfig.ARM_DATA, 'ARM_DATA_CONTAINER')) % For backward compatibility reasons.
-    if isfield(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}.KINEMATICS, parameter)
-        output = str2num(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}.KINEMATICS.(parameter).Text);
-        return
-    elseif isfield(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}, parameter)
-        %% remove this condition when phiMin and phiMax are moved into ARM_DATA_CONTAINER
-        output = str2num(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}.(parameter).Text);
-        return
-    else
-        warning(strcat(parameter, ' - Parameter name is not in configuration structure.'));
-        %disp(fieldnames(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}.KINEMATICS));
-    end
-else
-    %% we should just dump this condition.
-    for ii = 1:sum(regexpcmp(fieldnames(CobraConfig.ARM_DATA),'ARM_DATA_*')) % Count all struct that start with the name ARM_DATA_
-        ARM_DATA_n = sprintf('ARM_DATA_%d', ii);
-        if(pID ==  str2num(CobraConfig.ARM_DATA.(ARM_DATA_n).DATA_HEADER.Positioner_Id.Text))
-            if isfield(CobraConfig.ARM_DATA.(ARM_DATA_n).KINEMATICS, parameter)
-                output = str2num(CobraConfig.ARM_DATA.(ARM_DATA_n).KINEMATICS.(parameter).Text);
-                return
-            elseif isfield(CobraConfig.ARM_DATA.(ARM_DATA_n), parameter)
-                output = str2num(CobraConfig.ARM_DATA.(ARM_DATA_n).(parameter).Text);
-                return
-            else
-                warning(strcat(parameter, ' - Parameter name is not in configuration structure.'));
-            end
+    for jj = 1:length(containers)
+        kk = containers(jj);
+        if isfield(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}.KINEMATICS, parameter)
+            output(jj) = str2num(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}.KINEMATICS.(parameter).Text);
+        elseif isfield(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}, parameter)
+            %% remove this condition when phiMin and phiMax are moved into ARM_DATA_CONTAINER
+            output(jj) = str2num(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}.(parameter).Text);
+        else
+            warning(strcat(parameter, ' - Parameter name is not in configuration structure.'));
+            %disp(fieldnames(CobraConfig.ARM_DATA.ARM_DATA_CONTAINER{kk}.KINEMATICS));
         end
     end
-end 
-
+else
+    disp('Configuration structure appears malformed');
+end
 
 
