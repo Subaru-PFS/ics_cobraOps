4f890ddf772fc82fcdcbb10703468e6ea27d8820
Peter Mao <peterm@caltech.edu>
2018-02-14 14:23:42 -0800

    popped out addNoise from gt2

--- matlab/collisions/generateTrajectory2.m
+++ matlab/collisions/generateTrajectory2.m
@@ -177,17 +177,3 @@ end
 
 return
 end
-
-function noisy=addNoise(cumMap, fBE)
-% add noise to a cumulative map, according to the fractionBinError (fBE).
-
-    dataAxis = 2; % each cobras data is laid out in the 2nd axis
-
-    Map = diff(cumMap,1,dataAxis);
-    mapSize = size(Map);
-
-    noisy = Map .* mapFactor(fBE, mapSize);
-    noisy = cumsum([zeros(mapSize(1),1) Map], dataAxis);
-    % make last entry huge to eliminate over-runs
-    noisy(:,end) = 1e9;
-end
