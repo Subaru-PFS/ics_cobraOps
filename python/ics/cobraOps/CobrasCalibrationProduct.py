06ed42ea86ecf1e932a048a03c7087e16d683597
Craig Loomis <cloomis@astro.princeton.edu>
2018-05-16 18:15:16 -0400

    Keep motor properties from the XML file.

--- python/ics/cobraOps/CobrasCalibrationProduct.py
+++ python/ics/cobraOps/CobrasCalibrationProduct.py
@@ -59,6 +59,19 @@ class CobrasCalibrationProduct(AttributePrinter):
         self.L1 = np.empty(self.nCobras)
         self.L2 = np.empty(self.nCobras)
         
+        self.motorFreq1 = np.empty(self.nCobras)
+        self.motorFreq2 = np.empty(self.nCobras)
+        
+        self.motorOntimeFwd1 = np.empty(self.nCobras)
+        self.motorOntimeFwd2 = np.empty(self.nCobras)
+        self.motorOntimeRev1 = np.empty(self.nCobras)
+        self.motorOntimeRev2 = np.empty(self.nCobras)
+
+        self.motorOfftimeFwd1 = np.empty(self.nCobras)
+        self.motorOfftimeFwd2 = np.empty(self.nCobras)
+        self.motorOfftimeRev1 = np.empty(self.nCobras)
+        self.motorOfftimeRev2 = np.empty(self.nCobras)
+
         # Check if the data containers have information about the motor maps
         slowCalTable = dataContainers[0].find("SLOW_CALIBRATION_TABLE")
         
@@ -84,7 +97,7 @@ class CobrasCalibrationProduct(AttributePrinter):
             header = dataContainers[i].find("DATA_HEADER")
             self.moduleIds[i] = int(header.find("Module_Id").text)
             self.positionerIds[i] = int(header.find("Positioner_Id").text)
-            
+
             # Save some of the kinematics information
             kinematics = dataContainers[i].find("KINEMATICS")
             self.centers[i] = float(kinematics.find("Global_base_pos_x").text) + float(kinematics.find("Global_base_pos_y").text) * 1j
@@ -108,7 +121,21 @@ class CobrasCalibrationProduct(AttributePrinter):
                 fastCalTable = dataContainers[i].find("FAST_CALIBRATION_TABLE")
                 angularPositions = slowCalTable.find("Joint1_fwd_regions").text.split(",")[2:-1]
                 angularStep = float(angularPositions[1]) - float(angularPositions[0])
-                
+
+                # Get the motor properties: frequencies and stepsizes
+                self.motorFreq1[i] = float(header.find('Motor1_Run_Frequency').text)
+                self.motorFreq2[i] = float(header.find('Motor2_Run_Frequency').text)
+
+                self.motorOntimeFwd1[i] = float(kinematics.find('Link1_fwd_Duration').text)
+                self.motorOntimeRev1[i] = float(kinematics.find('Link1_rev_Duration').text)
+                self.motorOntimeFwd2[i] = float(kinematics.find('Link2_fwd_Duration').text)
+                self.motorOntimeRev2[i] = float(kinematics.find('Link2_rev_Duration').text)
+
+                self.motorOfftimeFwd1[i] = float(kinematics.find('Link1_fwd_Intervals').text)
+                self.motorOfftimeRev1[i] = float(kinematics.find('Link1_rev_Intervals').text)
+                self.motorOfftimeFwd2[i] = float(kinematics.find('Link2_fwd_Intervals').text)
+                self.motorOfftimeRev2[i] = float(kinematics.find('Link2_rev_Intervals').text)
+
                 # Get the cobra motors speeds in degrees per step
                 slowJoint1Fwd = slowCalTable.find("Joint1_fwd_stepsizes").text.split(",")[2:-1]
                 slowJoint1Rev = slowCalTable.find("Joint1_rev_stepsizes").text.split(",")[2:-1]
