5817b757e6e5046f9039228bcaeb5ec160fde783
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-06-20 13:25:12 +0200

    experimental setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name="cobraOps",
+setup(name="ics.cobraOps",
       #version="x.y",
       author="Javier Gracia Carpio",
       #author_email="",
       #description="",
       url="https://github.com/Subaru-PFS/ics_cobraOps/",
-      packages=find_packages(include=["cobraOps", "cobraOps.*"]),
+      packages=find_packages(include=["ics.cobraOps", "ics.cobraOps.*"]),
       zip_safe=True,
       license="GPLv3",
       install_requires=["numpy", "matplotlib"],
