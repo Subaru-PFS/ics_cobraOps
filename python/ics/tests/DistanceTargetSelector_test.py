a3fe2e9e039861d7aa13da9f59a19898de9a47b7
jagracar <jagracar@gmail.com>
2019-11-15 18:33:44 +0100

    Adds more unit tests for Distance and RandomTargetSelector classes

--- python/ics/tests/DistanceTargetSelector_test.py
+++ python/ics/tests/DistanceTargetSelector_test.py
@@ -4,7 +4,10 @@ Collection of unit tests for the DistanceTargetSelector class.
 
 """
 
+import numpy as np
+
 from ics.cobraOps.DistanceTargetSelector import DistanceTargetSelector
+from ics.cobraOps.RandomTargetSelector import RandomTargetSelector
 
 
 class TestDistanceTargetSelector():
@@ -12,5 +15,44 @@ class TestDistanceTargetSelector():
 
     """
 
-    def test_constructor(self, bench, targets):
-        DistanceTargetSelector(bench, targets)
+    def test_run_method(self, bench, targets):
+        # Create a DistanceTargetSelector
+        selector = DistanceTargetSelector(bench, targets)
+
+        # Run the complete target selection
+        selector.run()
+
+        # Get the selected targets
+        selectedTargets = selector.getSelectedTargets()
+
+        # Check that the result makes sense
+        assert selectedTargets.nTargets == bench.cobras.nCobras
+        assert np.sum(selectedTargets.notNull) > 0
+
+    def test_compare_with_random_selection(self, bench, targets):
+        # Create a DistanceTargetSelector
+        selector = DistanceTargetSelector(bench, targets)
+
+        # Run the complete target selection
+        selector.run()
+
+        # Get the selected targets
+        selectedTargets = selector.getSelectedTargets()
+
+        # Create a RandomTargetSelector
+        randomSelector = RandomTargetSelector(bench, targets)
+
+        # Run the complete target selection
+        randomSelector.run()
+
+        # Get the selected random targets
+        randomSelectedTargets = randomSelector.getSelectedTargets()
+
+        # Check that the average of the target distances is larger for the
+        # random selector method
+        cobraCenters = bench.cobras.centers
+        distances = np.abs(cobraCenters - selectedTargets.positions)
+        distances = distances[selectedTargets.notNull]
+        randomDistances = np.abs(cobraCenters - randomSelectedTargets.positions)
+        randomDistances = randomDistances[randomSelectedTargets.notNull]
+        assert np.mean(distances) < np.mean(randomDistances)
--- python/ics/tests/RandomTargetSelector_test.py
+++ python/ics/tests/RandomTargetSelector_test.py
@@ -4,7 +4,10 @@ Collection of unit tests for the RandomTargetSelector class.
 
 """
 
+import numpy as np
+
 from ics.cobraOps.RandomTargetSelector import RandomTargetSelector
+from ics.cobraOps.cobraConstants import NULL_TARGET_INDEX
 
 
 class TestRandomTargetSelector():
@@ -12,5 +15,46 @@ class TestRandomTargetSelector():
 
     """
 
-    def test_constructor(self, bench, targets):
-        RandomTargetSelector(bench, targets)
+    def test_randomizeAccessibleTargetsOrder_method(self, bench, targets):
+        # Create a RandomTargetSelector
+        selector = RandomTargetSelector(bench, targets)
+
+        # Calculate the accessible targets for each cobra
+        selector.calculateAccessibleTargets()
+
+        # Randomize the accessible targets order
+        selector.randomizeAccessibleTargetsOrder()
+
+        # Check that the accessible targets are not ordered by distance
+        orderedByDistance = True
+
+        for i in range(bench.cobras.nCobras):
+            # Get the accessible target indices and distances for this cobra
+            indices = selector.accessibleTargetIndices[i]
+            distances = selector.accessibleTargetDistances[i]
+            validTargets = indices != NULL_TARGET_INDEX
+
+            # Check if the distances are ordered
+            nTargets = np.sum(validTargets)
+            orderedByDistance = np.all(
+                np.sort(distances[:nTargets]) == distances[:nTargets])
+
+            # Exit the loop if the targets are not ordered by distance
+            if not orderedByDistance:
+                break
+
+        assert not orderedByDistance
+
+    def test_run_method(self, bench, targets):
+        # Create a RandomTargetSelector
+        selector = RandomTargetSelector(bench, targets)
+
+        # Run the complete target selection
+        selector.run()
+
+        # Get the selected targets
+        selectedTargets = selector.getSelectedTargets()
+
+        # Check that the result makes sense
+        assert selectedTargets.nTargets == bench.cobras.nCobras
+        assert np.sum(selectedTargets.notNull) > 0
