dd581db22f4de8b8d9c57222247e4704efa54dba
Javier Gracia Carpio <jagracar@gmail.com>
2025-08-05 16:18:49 +0200

    Improves setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name="ics.cobraOps",
-      #version="x.y",
+      version="1.0.0",
       author="Javier Gracia Carpio",
-      #author_email="",
-      #description="",
+      author_email="jgracia@mpe.mpg.de",
+      description="A pacakage that simulates the movements of cobras and calculates their possible collisions.",
       url="https://github.com/Subaru-PFS/ics_cobraOps/",
       packages=find_packages("python"),
       package_dir={'':'python'},
